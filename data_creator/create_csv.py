37ce02caac92d75724d1955a49bc66e86ad2c18a
Jaehyun-Jeong <wogus0948@naver.com>
2023-02-14 07:35:18 +0000

    creating onehot feature

--- data_creator/create_csv.py
+++ data_creator/create_csv.py
@@ -1,5 +1,6 @@
 import random
 import operator
+import pandas as pd
 
 
 class ArithmeticDataCreator():
@@ -8,37 +9,57 @@ class ArithmeticDataCreator():
             self,
             path: str,
             data_size: int,
+            onehot: bool = False,
             ):
 
         self._path = path
         self._data_size = data_size
+        self._onehot = onehot
 
+        # columns to create dataframe
         self._columns = ["first_number", "oprator", "second_number", "answer"]
+
+        # operator string to python operator func
         self._operators = {
                 '+': operator.add,
                 '-': operator.sub,
                 '*': operator.mul,
-                '/': operator.truediv,
                 }
 
+    # create random data
     def __rand_data(self):
+
+        # get random integer from 1 to 10
         first_number = random.randint(1, 10)
         second_number = random.randint(1, 10)
+
+        operator_list = list(self._operators.keys())
+
+        # get random opeerator and create 3-tuple
         features = (
                 first_number,
-                random.choice(['+', '-', '*', '/']),
+                random.choice(operator_list),
                 second_number)
+
+        if self._onehot:
+            pass
+
+        # calulate answer
         answer = self._operators[features[1]](first_number, second_number)
 
+        # stack answer to features and return
         return features + (answer,)
 
+    # create random dataframe
     def dataframe(self):
-        return self.__rand_data()
+        return pd.DataFrame(
+                [self.__rand_data() for i in range(self._data_size)],
+                columns=self._columns,
+                )
 
 
 if __name__ == "__main__":
 
     data_creator = ArithmeticDataCreator("./data.csv", 100)
 
-    for i in range(100):
-        print(data_creator.dataframe())
+    print(data_creator.dataframe())
