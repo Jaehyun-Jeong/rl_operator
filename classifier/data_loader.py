3c48407d599779d1b26d6f2178f313d1d72ed603
Jaehyun-Jeong <wogus0948@naver.com>
2023-02-27 05:11:51 +0000

    fixed

--- classifier/data_loader.py
+++ classifier/data_loader.py
@@ -6,6 +6,9 @@ from torch.utils.data import Dataset, DataLoader
 
 class OperationDataset(Dataset):
 
+    # It's because smallest number is -9
+    INDEXING_GAP = 9
+
     def __init__(
             self,
             x: torch.Tensor,
@@ -14,6 +17,7 @@ class OperationDataset(Dataset):
 
         self.data = x
         self.labels = y
+        self.indexing()
 
         super().__init__()
 
@@ -25,6 +29,10 @@ class OperationDataset(Dataset):
         y = self.labels[idx]
 
         return x, y
+
+    def indexing(self):
+        for i in range(self.labels.size(0)):
+            self.labels[i] = self.labels[i] + self.INDEXING_GAP
 
 
 def get_loaders(config):
@@ -40,23 +48,23 @@ def get_loaders(config):
     indices = torch.randperm(x.size(0))
     train_x, valid_x = torch.index_select(
             x,
-            dim = 0,
-            index = indices
-        ).split([train_cnt, valid_cnt], dim = 0)
+            dim=0,
+            index=indices
+        ).split([train_cnt, valid_cnt], dim=0)
     train_y, valid_y = torch.index_select(
             y,
-            dim = 0,
-            index = indices
-        ).split([train_cnt, valid_cnt], dim = 0)
+            dim=0,
+            index=indices
+        ).split([train_cnt, valid_cnt], dim=0)
 
     train_loader = DataLoader(
-            dataset = OperationDataset(train_x, train_y),
-            batch_size = config.batch_size,
-            shuffle = True)
+            dataset=OperationDataset(train_x, train_y),
+            batch_size=config.batch_size,
+            shuffle=True)
     valid_loader = DataLoader(
-            dataset = OperationDataset(valid_x, valid_y),
-            batch_size = config.batch_size,
-            shuffle = True)
+            dataset=OperationDataset(valid_x, valid_y),
+            batch_size=config.batch_size,
+            shuffle=True)
 
     # Load Test
 
@@ -65,8 +73,8 @@ def get_loaders(config):
     test_x = torch.tensor(df.iloc[:, :-1].values)
     test_y = torch.tensor(df.iloc[:, -1].values)
     test_loader = DataLoader(
-            dataset = OperationDataset(test_x, test_y),
-            batch_size = config.batch_size,
-            shuffle = False)
+            dataset=OperationDataset(test_x, test_y),
+            batch_size=config.batch_size,
+            shuffle=False)
 
     return train_loader, valid_loader, test_loader
